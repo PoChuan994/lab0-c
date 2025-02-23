bdeb6aa685b496e1f1b53ee69c807fc7e30e0df0
PoChuan994 <pochuan994@gmail.com>
2025-02-24 01:06:57 +0800

    Implement 'q_free' function
    Use the Linux Kernel API to free a queue storage.
    
    Change-Id: Ife8b54dc81e318ff40569c21ff8276d941e02c4d

--- queue.c
+++ queue.c
@@ -21,7 +21,18 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *current, *safe;
+    list_for_each_safe (current, safe, head) {
+        element_t *release_pos = list_entry(current, element_t, list);
+        q_release_element(release_pos);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
