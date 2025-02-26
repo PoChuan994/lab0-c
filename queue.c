8c3a5746339a2416a14ce28942dd764e05e36367
PoChuan994 <pochuan994@gmail.com>
2025-02-26 16:23:04 +0800

    Implement 'q_remove_tail'
    Remove a tail element in a\
    queue using the Linux\
    Kernel API.
    
    Change-Id: Ie2c12a3fe58ab3669fc726d5f4a75ec8d4633174

--- queue.c
+++ queue.c
@@ -92,7 +92,17 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) /* input validation */
+        return NULL;
+    struct list_head *node = head->prev;
+    element_t *element = list_entry(node, element_t, list);
+
+    list_del(node);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = 0;
+    }
+    return element;
 }
 
 /* Return number of elements in queue */
